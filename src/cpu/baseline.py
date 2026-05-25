026a12c7a3b119959ee259dfaa31fe9c158cbc00
gsantosPerson <gustavo.oliveira.person@gmail.com>
2026-05-25 15:29:24 -0300

    Adição da função minimoCpu

--- src/cpu/baseline.py
+++ src/cpu/baseline.py
@@ -17,9 +17,19 @@ def maximoCpu(v):
 
     return maior
 
+def minimoCpu(v):
+    menor = v[0]
+
+    for i in range(1, len(v)):
+        if v[i] < menor:
+            menor = v[i]
+    
+    return menor
+
 v = np.array([1, 2, 3, 4, 5])
 
 print("Vetor:", v)
 
 print("Soma:", somaCpu(v))
 print("Máximo:", maximoCpu(v))
+print("Mínimo:", minimoCpu(v))
