900e8250f723daa5050865e4cec2637cf7a4aa16
gsantosPerson <gustavo.oliveira.person@gmail.com>
2026-05-25 15:22:16 -0300

    Criação do vetor e implementação da função soma

--- src/cpu/baseline.py
+++ src/cpu/baseline.py
@@ -1,1 +1,15 @@
+import numpy as np
 
+def soma_cpu(v):
+    soma = 0
+
+    for i in range(len(v)):
+        soma += v[i]
+
+    return soma
+
+v = np.array([1, 2, 3, 4, 5])
+
+print("Vetor:", v)
+
+print("Soma:", soma_cpu(v))
